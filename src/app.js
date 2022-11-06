3e98d697c7ca10234d76a68d8f81d17af686a682
Leonpsilv <leonardo.pin.s2002@gmail.com>
2022-11-06 18:17:59 -0400

    components

--- src/app.js
+++ src/app.js
@@ -1,44 +1,36 @@
 'use strict'
 
 import React, { Component } from 'react'
+import Search from './components/Search'
+import UserInfo from './components/User-info'
+import Actions from './components/Actions'
+import Repos from './components/Repos'
 
 class App extends Component {
   render () {
     return (
       <div className='app'>
-        <div className='search'>
-          <input type='search' placeholder='Digite o nome do usuário no GitHub'/>
-        </div>
-        <div className='user-info'>
-          <img src='https://avatars.githubusercontent.com/u/102977531?v=4' />
-          <h1><a href='https://github.com/Leonpsilv'>Leonardo Silva </a></h1>
+        <Search />
+        <UserInfo />
+        <Actions />
 
-          <ul className='repos-info'>
-            <li>- Repositórios: 8</li>
-            <li>- Seguidores: 2</li>
-            <li>- Seguindo: 2</li>
-          </ul>
+        <Repos
+          className='repos'
+          title='Repositórios'
+          repos={[{
+            name: 'Nome do repositório',
+            link: '#'
+          }]}
+        />
 
-          <div className='actions'>
-            <button>Ver repositórios</button>
-            <button>Ver favoritos</button>
-          </div>
-
-          <div className='repos'>
-            <h2>Repositórios:</h2>
-            <ul>
-              <li><a href='#'>Nome do Repositório</a></li>
-            </ul>
-          </div>
-
-          <div className='starred'>
-            <h2>Favoritos:</h2>
-            <ul>
-              <li><a href='#'>Nome do Repositório</a></li>
-            </ul>
-          </div>
-
-        </div>
+        <Repos
+          className='starred'
+          title='Favoritos'
+          repos={[{
+            name: 'Nome do repositório',
+            link: '#'
+          }]}
+        />
       </div>
     )
   }
---
+++ src/components/User-info.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import React from 'react'
+
+const UserInfo = () => (
+  <div className='user-info'>
+    <img src='https://avatars.githubusercontent.com/u/102977531?v=4' />
+    <h1><a href='https://github.com/Leonpsilv'>Leonardo Silva </a></h1>
+    <ul className='repos-info'>
+      <li>- Repositórios: 8</li>
+      <li>- Seguidores: 2</li>
+      <li>- Seguindo: 2</li>
+    </ul>
+  </div>
+)
+
+export default UserInfo
